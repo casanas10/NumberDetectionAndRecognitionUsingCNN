dfb61c069c284ccf7b0e698dea4e87f0b9a0af90
Alejandro Casanas <casanas10@gmail.com>
2018-11-11 18:57:41 -0500

    set up environment

---
+++ final_proj.py
@@ -0,0 +1,6 @@
+import cv2
+import os
+
+def load_images():
+
+    print('loading')
---
+++ run.py
@@ -0,0 +1,45 @@
+import cv2
+import os
+
+
+# file paths to images
+
+
+def run():
+
+    #load images
+
+    #select percentage of data for training and testing
+
+    #split dataset
+
+
+    #calculate random choice
+
+
+    #BUILD the CNN
+
+    #define hyperparameters
+
+
+    #####TRAINING
+
+    #train model
+
+    #get predictions
+
+    #calculate accuracy of training
+
+
+    #####EVALUATE
+
+
+    #####TESTING
+
+
+
+    print('here')
+
+
+if __name__ == "__main__":
+    run()
